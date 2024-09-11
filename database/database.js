035a3ba3f7f1911ead4db14178ccd9be7acb9078
“JoO17pt“ <joao.pedro.c.araujo@gmail.com>
2024-09-11 14:23:49 +0100

    Version 3

--- database/database.js
+++ database/database.js
@@ -2,7 +2,7 @@ import Sequelize from "sequelize";
 
 const connection = new Sequelize('APPDOZE','root','root',{
     // host: 'localhost',
-    host: 'tcp://0.tcp.eu.ngrok.io',
+    host: '0.tcp.eu.ngrok.io',
     port: '18825',
     dialect: 'mysql',
     logging: true
