1b05eb83767e78cc24cbae34673e587996cf6f62
“JoO17pt“ <joao.pedro.c.araujo@gmail.com>
2024-09-15 00:45:08 +0100

    Version 6

--- controllers/HomeController.js
+++ controllers/HomeController.js
@@ -1,9 +1,25 @@
-import dotenv from 'dotenv'
-dotenv.config({path: './config/config.env'})
+import dotenv from "dotenv";
+import connection from "../database/database.js";
+
+dotenv.config({ path: "./config/config.env" });
 
 const home = (req, res) => {
-    res.render("home", {user: req.session.user, message: null});
-  }
-;
-
+  connection
+    .query(`SELECT COUNT(id) AS NumberLawSuits FROM lawSuits`, { raw: true })
+    .then((numberLawSuits) => {
+      connection
+        .query(
+          `SELECT STR_TO_DATE(CONCAT(LEFT(lawSuits.stringDate, 2), '/', MID(lawSuits.stringDate, 4, 2), '/', RIGHT(lawSuits.stringDate, 4)), '%d/%m/%Y') AS stringDate FROM lawSuits ORDER BY stringDate DESC LIMIT 1`,
+          { raw: true }
+        )
+        .then((lastLawSuit) => {
+          res.render("home", {
+            user: req.session.user,
+            numberLawSuits: numberLawSuits[0][0].NumberLawSuits,
+            lastLawSuit: lastLawSuit[0][0].stringDate,
+            message: null,
+          });
+        });
+    });
+};
 export default home;
--- middlewares/sessionInit.js
+++ middlewares/sessionInit.js
@@ -1,3 +1,5 @@
+import connection from "../database/database.js";
+
 function sessionInit(req, res, next) {
   if (
     req.session.user != undefined &&
@@ -11,7 +13,23 @@ function sessionInit(req, res, next) {
   } else if (req.session.user == undefined) {
     res.redirect("/users/login");
   } else {
-    res.render("home", {user: req.session.user, message: true});
+    connection
+    .query(`SELECT COUNT(id) AS NumberLawSuits FROM lawSuits`, { raw: true })
+    .then((numberLawSuits) => {
+      connection
+        .query(
+          `SELECT STR_TO_DATE(CONCAT(LEFT(lawSuits.stringDate, 2), '/', MID(lawSuits.stringDate, 4, 2), '/', RIGHT(lawSuits.stringDate, 4)), '%d/%m/%Y') AS stringDate FROM lawSuits ORDER BY stringDate DESC LIMIT 1`,
+          { raw: true }
+        )
+        .then((lastLawSuit) => {
+          res.render("home", {
+            user: req.session.user,
+            numberLawSuits: numberLawSuits[0][0].NumberLawSuits,
+            lastLawSuit: lastLawSuit[0][0].stringDate,
+            message: true,
+          });
+        });
+    });
   }
 }
 
