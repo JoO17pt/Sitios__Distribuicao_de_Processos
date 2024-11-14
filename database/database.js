bfa5b2a501d67f5cb6afd9ee4de36db6f2b8cd6b
“JoO17pt“ <joao.pedro.c.araujo@gmail.com>
2024-11-14 22:38:49 +0000

    Version 12

--- database/database.js
+++ database/database.js
@@ -6,9 +6,9 @@ dotenv.config({ path: "./config/config.env" });
 const connection = new Sequelize('APPDOZE','root','root',{
 // const connection = new Sequelize('APPDOZE',process.env.DBUSER,process.env.DBPASS,{
     // host: 'localhost',
-    // host: process.env.HOST,
+    host: process.env.HOST,
     // port: process.env.PORT,
-    host: '0.tcp.eu.ngrok.io',
+    // host: '0.tcp.eu.ngrok.io',
     port: '15921',
     dialect: 'mysql',
     logging: true
