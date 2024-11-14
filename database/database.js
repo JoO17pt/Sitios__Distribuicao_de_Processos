cdce56464ecd797a25c6711652e033b4d73097f0
“JoO17pt“ <joao.pedro.c.araujo@gmail.com>
2024-11-14 22:41:42 +0000

    Version 13

--- database/database.js
+++ database/database.js
@@ -3,8 +3,8 @@ import Sequelize from "sequelize";
 import dotenv from "dotenv";
 dotenv.config({ path: "./config/config.env" });
 
-const connection = new Sequelize('APPDOZE','root','root',{
-// const connection = new Sequelize('APPDOZE',process.env.DBUSER,process.env.DBPASS,{
+// const connection = new Sequelize('APPDOZE','root','root',{
+const connection = new Sequelize('APPDOZE',process.env.DBUSER,process.env.DBPASS,{
     // host: 'localhost',
     host: process.env.HOST,
     // port: process.env.PORT,
